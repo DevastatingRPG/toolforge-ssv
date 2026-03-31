dff1121c0f8e87c92fadeb0e16d227986fb0ebba
Shubham Sarkar <devastatingrpg@gmail.com>
2026-03-31 22:38:35 +0530

    feat: changes so far, reworked observation so far

---
+++ toolforge_env/__init__.py
@@ -0,0 +1,16 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Toolforge Env Environment."""
+
+from .client import ToolforgeEnv
+from .models import ToolforgeAction, ToolforgeObservation
+
+__all__ = [
+    "ToolforgeAction",
+    "ToolforgeObservation",
+    "ToolforgeEnv",
+]
---
+++ toolforge_env/server/__init__.py
@@ -0,0 +1,11 @@
+# Copyright (c) Meta Platforms, Inc. and affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+"""Toolforge Env environment server components."""
+
+from .toolforge_env_environment import ToolforgeEnvironment
+
+__all__ = ["ToolforgeEnvironment"]
